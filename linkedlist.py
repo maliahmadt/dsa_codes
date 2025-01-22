3a190ceef68809918b005c14b285499c05302aa1
maliahmadt <muhammadaliahmadt@gmail.com>
2025-01-22 19:57:57 +0500

    added AddLast function

--- linkedlist.py
+++ linkedlist.py
@@ -12,11 +12,22 @@ class list:
             self.first = newnode
         elif self.first != None:
             newnode = node(data)
-            newnode.next = self.first
+            newnode.addr = self.first
             self.first = newnode
+    def AddLast(self,data):
+        itr = self.first
+        while True:
+            if itr.addr != None:
+                itr = itr.addr
+            else:
+                break
+        newnode = node(data)
+        itr.addr = newnode
         
         
         
 L = list(None)
 L.Addfirst(2)
 L.Addfirst(1)
+L.AddLast(3)
+L.AddLast(4)
