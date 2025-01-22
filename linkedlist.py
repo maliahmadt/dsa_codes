d2d614a88e6f5f2b3e09134daa18e7c5c77854d0
maliahmadt <muhammadaliahmadt@gmail.com>
2025-01-22 19:33:22 +0500

    commit

--- linkedlist.py
+++ linkedlist.py
@@ -2,3 +2,15 @@ class node:
     def __init__(self, data,addr):
         self.data = data
         self.addr = addr
+        
+class list:
+    def __init__(self,first):
+        self.first = first
+    def Addfirst(self,data):
+        newnode = node(data,None)
+        self.first = newnode
+        
+        
+        
+L = list(None)
+L.Addfirst(5)
